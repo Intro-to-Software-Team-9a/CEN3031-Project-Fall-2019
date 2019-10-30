4d6d7918887d6ae799411b15edd51a226e063b2b
Gary Gurlaskie <garygurlaskie@gmail.com>
2019-10-30 09:20:34 -0400

    added basic tests for models

---
+++ server/tests/helpers/mockdata.js
@@ -0,0 +1,16 @@
+module.exports = {
+  account1: {
+    email: 'test@gmail.com',
+    passwordHash: 'test',
+  },
+  profile1: {
+    name: 'Example User',
+  },
+  document1: {
+    text: 'Hello, my name is Example User',
+  },
+  template1: {
+    title: 'Introduction',
+    template: 'Hello, my name is {{ name }}',
+  },
+};
