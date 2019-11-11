ab837e2dad1c80b73fd4371650be846264f9d2c9
harrison-hope <hhope000@gmail.com>
2019-11-11 17:26:45 -0500

    Added opening pdf in new tab

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -6,11 +6,6 @@ import axios from 'axios';
 import printJS from 'print-js';
 const FileDownload = require('js-file-download');
 
-function openPDF(pdf){
-  window.open(pdf);
-  return false;
-}
-
 function Home() {
   var id = "5dc884c309be682148ecc86c";
 
@@ -20,8 +15,9 @@ function Home() {
       <script src="print.js"></script>
       <p>This is the app home page.</p>
       <p><Link to="/create-account">Create an account</Link></p>
-	    <p><button type="button" onClick={() => axios.get('/api/pdf/' + id ).then((response) => {console.log(response); FileDownload(response.data, 'sample.pdf')})}>Get PDF</button></p>
-      <p><button type="button" onClick={() => axios.get('/api/pdf/' + id ).then((response) => {let blob = new Blob([response.data], {type: 'application/pdf'}); openPDF([response.data])})}>Print PDF</button></p>
+	    <p><button type="button" onClick={() => axios.get('/api/pdf/' + id ).then((response) => {console.log(response); FileDownload(response.data, 'sample.pdf')})}>Download PDF</button></p>
+      <p><button type="button" onClick={() => axios.get('/api/pdf/' + id ).then((response) => {})}>Print PDF</button></p>
+      <p><button type="button" onClick={() => window.open('http://localhost:5000/api/pdf/'+id, '_blank')}>Open PDF</button></p>
       <a href={axios.get('/api/pdf/' + id).response} download>Download</a>
       <p><Link to="/create-template">Create a Document</Link></p>
     </Container>
