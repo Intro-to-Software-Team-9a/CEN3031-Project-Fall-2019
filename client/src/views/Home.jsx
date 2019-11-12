e3e86c00c7c2a64e79d8c0b579507ec7ebd9ff2c
Gary Gurlaskie <garygurlaskie@gmail.com>
2019-11-11 21:48:47 -0500

    resolved merge conflict

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Home.css';
 
 function Home() {
   return (
     <Container className="pt-4">
-      <p style={{ color: '#F5FFFA' }}>This is the app home page.</p>
-      <p><Link to="/create-account">Create an account</Link></p>
-      <p><Link to="/create-template">Create a Document</Link></p>
-      <p><Link to="/questionnaire">View Questionnaire</Link></p>
+      <Link to="/questionnaire"><Button variant="outline-dark">Get Started</Button></Link>
     </Container>
   );
 }
