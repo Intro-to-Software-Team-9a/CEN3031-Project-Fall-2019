2f28b241aaaa566ac198f4640459dc6395460fe4
Gary Gurlaskie <garygurlaskie@gmail.com>
2019-11-11 20:15:29 -0500

    removed commented-out styles

--- client/src/views/Catalog.jsx
+++ client/src/views/Catalog.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import Cart from '../components/Cart';
 import AddToCart from '../components/AddToCart';
 import { read } from 'fs';
-//import './style.css';
+import './style.css';
 
 
 
