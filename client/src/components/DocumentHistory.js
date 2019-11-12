b63cf09f27683529c75fd92aa52acb038a18130e
harrison-hope <hhope000@gmail.com>
2019-11-11 23:24:35 -0500

    Rebased, fixed errors (hopefully didnt make more) api/documents not working

--- client/src/components/DocumentHistory.js
+++ client/src/components/DocumentHistory.js
@@ -23,8 +23,8 @@ function DocumentHistory({ activeTemplate, documents }) {
             </Col>
             <Col>
               <ButtonToolbar>
-                <Button variant="outline-dark" className="mr-2"><span className="mr-1"><GetAppIcon /></span>Download</Button>
-                <Button variant="outline-dark" className="mr-2"><span className="mr-1"><PrintIcon />ˇ</span>Print</Button>
+                <Button variant="outline-dark" className="mr-2" onClick={() => window.open('http://localhost:5000/api/pdf/'+document._id, '_blank')}><span className="mr-1"><GetAppIcon /></span>View</Button>
+                {/*<Button variant="outline-dark" className="mr-2"><span className="mr-1"><PrintIcon />ˇ</span>Print</Button>*/}
               </ButtonToolbar>
             </Col>
           </Row>
--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -3,23 +3,14 @@ import { Container, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Home.css';
 import axios from 'axios';
-import printJS from 'print-js';
-const FileDownload = require('js-file-download');
 
 function Home() {
-  var id = "5dc884c309be682148ecc86c";
 
   return (
     <Container className="pt-4">
       <Link to="/questionnaire"><Button variant="outline-dark">Get Started</Button></Link>
-      <meta charset="utf-16"></meta>
-      <script src="print.js"></script>
       <p>This is the app home page.</p>
       <p><Link to="/create-account">Create an account</Link></p>
-	    <p><button type="button" onClick={() => axios.get('/api/pdf/' + id ).then((response) => {console.log(response); FileDownload(response.data, 'sample.pdf')})}>Download PDF</button></p>
-      <p><button type="button" onClick={() => axios.get('/api/pdf/' + id ).then((response) => {})}>Print PDF</button></p>
-      <p><button type="button" onClick={() => window.open('http://localhost:5000/api/pdf/'+id, '_blank')}>Open PDF</button></p>
-      <a href={axios.get('/api/pdf/' + id).response} download>Download</a>
       <p><Link to="/create-template">Create a Document</Link></p>
     </Container>
   );
