570c8a8ebc806c77339b35056b84914a88aa40a4
Thomas Allen <thomallen96@gmail.com>
2019-11-20 23:03:54 -0500

    Redirect to admin homepage if user is admin from Login.

--- client/src/views/Login.jsx
+++ client/src/views/Login.jsx
@@ -1,17 +1,40 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Container, Row, Col } from 'react-bootstrap';
 
 import LoginForm from '../components/LoginForm.jsx';
 
-export default function Login({ history }) {
-  return (
-    <Container className="pt-4">
-      <h1>Log In</h1>
-      <Row>
-        <Col className="col-4 pt-4">
-          <LoginForm onFinish={() => history.push('/profile-home')} />
-        </Col>
-      </Row>
-    </Container>
-  );
+class Login extends React.Component {
+  onLogin() {
+    var history = this.props.history;
+    var profile = this.props.profile;
+
+    if (profile.role.isAdmin) {
+      history.push('/admin');
+    }
+    else {
+      history.push('/profile-home');
+    }
+  }
+
+  render() {
+    return (
+      <Container className="pt-4">
+        <h1>Log In</h1>
+        <Row>
+          <Col className="col-4 pt-4">
+            <LoginForm onFinish={this.onLogin.bind(this)} />
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
+
+const mapStateToProps = (state) => ({
+  profile: state.profiles.profile
+});
+
+export default connect(
+  mapStateToProps
+)(Login);
