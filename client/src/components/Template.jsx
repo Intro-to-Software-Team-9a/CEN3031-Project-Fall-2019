98d6b8805472addb66905f45b00af4b4f34325cc
Thomas Allen <thomallen96@gmail.com>
2019-11-21 23:13:28 -0500

    Remove unused code.

--- client/src/components/Template.jsx
+++ client/src/components/Template.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Card, ListGroup } from 'react-bootstrap';
 import { formatCurrency } from '../utils/format';
 import DescriptionOutlinedIcon from '@material-ui/icons/DescriptionOutlined';
 import './Template.css';
