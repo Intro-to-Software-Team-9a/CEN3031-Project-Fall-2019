5f3fe40b8a52adf88ad6871cbf541eb440137c25
Renzo Pretto <renzopretto20000@gmail.com>
2019-12-10 16:28:56 -0500

    added config.example

---
+++ client/src/config/config.example.js
@@ -0,0 +1,10 @@
+/* eslint-disable linebreak-style */
+// This file holds any configuration variables we may need
+// 'config.js' is usually ignored by git to protect sensitive information,
+// such as your database's username and password
+
+module.exports = {
+  paypal: {
+    clientID: '',
+  },
+};
