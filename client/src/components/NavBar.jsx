9d9a4115783f54e6d8011f839839e9d45b4b0e38
grantreboucas <grant.reboucas97@gmail.com>
2019-12-09 22:12:29 -0500

    Added Langauge and formating for language

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -19,7 +19,7 @@ function NavBar({ isLoggedIn, name, doLogout }) {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto"></Nav>
         <Nav className="mr-sm-2">
-        <LinkContainer to={Routes.TERMS_CONDITIONS}><Nav.Link>Disclaimer</Nav.Link></LinkContainer>
+        <LinkContainer to={Routes.TERMS_CONDITIONS}><Nav.Link>Terms and Conditions</Nav.Link></LinkContainer>
           {isLoggedIn
             ? <React.Fragment>
               <LinkContainer to={Routes.PROFILE_HOME}><Nav.Link>{name}</Nav.Link></LinkContainer>
